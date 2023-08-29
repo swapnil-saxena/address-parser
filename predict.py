43c8033a0852280c6da04af52860e36b395d3024
Asjon <asjon.dalipaj@gmail.com>
2023-08-29 10:12:12 +0200

    Adapted to new test and train data

--- predict.py
+++ predict.py
@@ -1,123 +1,64 @@
 import spacy
-from spacy.tokens import DocBin
-import pandas as pd
-import re
-# pd.set_option('display.max_colwidth', -1)
 
-def massage_data(address):
-    '''Pre process address string to remove new line characters, add comma punctuations etc.'''
-    cleansed_address = re.sub(r'(,)(?!\s)', ', ', address)  # Add comma after comma without space
-    cleansed_address = re.sub(r'(\\n)', ', ', cleansed_address)  # Replace newline characters with comma and space
-    cleansed_address = re.sub(r'(?!\s)(-)(?!\s)', ' - ', cleansed_address)  # Add space around hyphens
-    cleansed_address = re.sub(r'\.', '', cleansed_address)  # Remove periods
-    cleansed_address = re.sub(r'\'', '', cleansed_address)  # Remove single quotes
-    cleansed_address = re.sub(r'[\(\)]', '', cleansed_address)  # Remove parentheses
-    print("After cleanse address: " + cleansed_address)
-    return cleansed_address
+nlp=spacy.load("output/models/model-best")
 
-def get_address_span(address=None,address_component=None,label=None):
-    '''Search for specified address component and get the span.
-    Eg: get_address_span(address="221 B, Baker Street, London",address_component="221",label="BUILDING_NO") would return (0,2,"BUILDING_NO")'''
+address_list = [
+    "Pieter de Hoochstraat 9 A 1071ED Amsterdam",
+    "Googweg 8 A 1399ER Muiderberg",
+    "Bloemenhof 51 1906XV Limmen",
+    "Prinses Alexialaan 5 * 2135VH Hoofddorp",
+    "Kempen 57 2036EK Haarlem",
+    "Aurikelstraat 55 1032AR Amsterdam",
+    "Prattenburg 403 2036SM Haarlem",
+    "Nieuwstraat 80 . 1381XV Weesp",
+    "Vissersdijk 52 1601LR Enkhuizen",
+    "Kanaalstraat 61 RD. 1975BB IJmuiden",
+    "Goudplevier 21 * 1191VP Ouderkerk aan de Amstel",
+    "J. Bijhouwersstraat 71 1992JV Velserbroek",
+    "Sint-Jorisveld 11 2023GD Haarlem",
+    "Krijn Taconiskade 469 1087HW Amsterdam 1000€",
+    "Amsteldijk 59 D 1074HX Amsterdam",
+    "Weldam 8 1081HN Amsterdam",
+    "Peltenburgstraat 53 2033ES Haarlem",
+    "Vierwindenstraat 78 1 1013LA Amsterdam",
+    "Revaleiland 442 + PP 1014ZG Amsterdam",
+    "'s-Gravelandseweg 6 1381HH Weesp",
+    "Bella Vistastraat 298 1096GM Amsterdam",
+    "IJdok 109 1013 MM Amsterdam",
+    "Huigsloterdijk 381 huur 2158 LR Buitenkaag",
+    "Cattenhagestraat 21 1411 CR Naarden",
+    "Balboaplein 84 1057 VS Amsterdam",
+    "Stroet 115 1744 GM Sint Maarten",
+    "Kolksteeg 3 2 1012 PT Amsterdam",
+    "Bijlmerplein 858 S7 1102 ME Amsterdam",
+    "Bijlmerplein 858 U6 1102 ME Amsterdam",
+    "Laurens Reaellaan 8 2 2024 BE Haarlem",
+    "Witte Herenstraat 25 2011 NT Haarlem",
+    "Pieter Goosstraat 10 1018 LA Amsterdam",
+    "Herengracht 14 1382 AE Weesp",
+    "Struikheidelaan 69 1213 WZ Hilversum",
+    "Prins Hendriklaan 70 1261AJ Blaricum",
+    "Prins Hendriklaan 74 1261 AJ Blaricum",
+    "Prins Hendriklaan 64 1261 AJ Blaricum",
+    "Prins Hendriklaan 78 1261 AJ Blaricum",
+    "Prins Hendriklaan 76 1261 AJ Blaricum",
+    "Prins Hendriklaan 68 1261 AJ Blaricum",
+    "Prins Hendriklaan 58 1261 AJ Blaricum",
+    "Prins Hendriklaan 54 1261 AJ Blaricum",
+    "Prins Hendriklaan 48 1261 AJ Blaricum",
+    "Prins Hendriklaan 66 1261 AJ Blaricum",
+    "Prins Hendriklaan 40 1261 AJ Blaricum",
+    "Prins Hendriklaan 30 1261 AJ Blaricum",
+    "VOC-kade 314 1018 LG 100€. Amsterdam",
+    "Noordereinde 12 1243 JG 's-Graveland",
+    "van Beekstraat 124 1121 NT Landsmeer"
+]
 
-    if pd.isna(address_component) or str(address_component)=='nan':
-        pass
-    else:
-        # print(f'- full address - {address}')
-        address_component = re.sub(r'(?!\s)(-)(?!\s)',' - ', address_component) # Put space before and after the hyphen
-        address_component = re.sub(r'\.', '', address_component)  # Remove periods from address_component
-        address_component = re.sub(r'\'', '', address_component)  # Remove single quotes from address_component
-        address_component = re.sub(r'[\(\)]', '', address_component)  # Remove parentheses from address_component
-        # print(address_component3)
-        if label == 'CITY' or label == 'ADDITION':
-            print(f'looking for {address_component} into {address}')
-            pattern = rf'\b(?:{address_component})\b'
-            matches = re.finditer(pattern, address)
-            span = None
-            for match in matches:
-                print(f'match found: {match}')
-                span = match
-            print(f'city or addition - found: {span}')
-        else:
-            print(f'looking for {address_component} into {address}')
-            span = re.search('\\b(?:' + address_component + ')\\b', address)
-            print(f'else - found: {span}')
-        return (span.start(),span.end(),label)
-
-def extend_list(entity_list,entity):
-    if pd.isna(entity):
-        return entity_list
-    else:
-        entity_list.append(entity)
-        return entity_list
-
-def create_entity_spans(df,tag_list):
-    '''Create entity spans for training/test datasets'''
-    df['full_address']=df['full_address'].apply(lambda x: massage_data(x))
-    # df['full_address']=df['full_address']
-    df["straatTag"]=df.apply(lambda row:get_address_span(address=row['full_address'],address_component=row['straat'],label='STREET'),axis=1)
-    df["huisnummerTag"]=df.apply(lambda row:get_address_span(address=row['full_address'],address_component=row['huisnummer'],label='HOUSE_NUMBER'),axis=1)
-    df["huisletterTag"]=df.apply(lambda row:get_address_span(address=row['full_address'],address_component=row['huisletter'],label='HOUSE_LETTER'),axis=1)
-    df["huistoevoegingTag"]=df.apply(lambda row:get_address_span(address=row['full_address'],address_component=row['huistoevoeging'],label='ADDITION'),axis=1)
-    df["postcodeTag"]=df.apply(lambda row:get_address_span(address=row['full_address'],address_component=row['postcode'],label='POSTAL_CODE'),axis=1)
-    df["cityTag"]=df.apply(lambda row:get_address_span(address=row['full_address'],address_component=row['woonplaats'],label='CITY'),axis=1)
-    df['EmptySpan']=df.apply(lambda x: [], axis=1)
-
-    for i in tag_list:
-        df['EntitySpans']=df.apply(lambda row: extend_list(row['EmptySpan'],row[i]),axis=1)
-        df['EntitySpans']=df[['EntitySpans','full_address']].apply(lambda x: (x[1], x[0]),axis=1)
-    return df['EntitySpans']
-
-def get_doc_bin(training_data,nlp):
-    '''Create DocBin object for building training/test corpus'''
-    # the DocBin will store the example documents
-    db = DocBin()
-    for text, annotations in training_data:
-        doc = nlp(text) #Construct a Doc object
-        ents = []
-        print(f'{text} - Annotations: {annotations}')
-        for start, end, label in annotations:
-            span = doc.char_span(start, end, label=label)
-            print(f'span: {span}')
-            ents.append(span)
-        doc.ents = ents
-        db.add(doc)
-    return db
-
-#Load blank Dutch model. This is needed for initializing a Document object for our training/test set.
-nlp = spacy.blank("nl")
-
-#Define custom entity tag list
-tag_list=["straatTag","huisnummerTag","huisletterTag","huistoevoegingTag","postcodeTag","cityTag"]
-
-###### Training dataset prep ###########
-# Read the training dataset into pandas
-# df_train=pd.read_csv(filepath_or_buffer="./corpus/dataset/us-train-dataset.csv",sep=",",dtype=str)
-df_train = pd.read_csv(r'corpus/dataset/distinct_addresses.csv',sep=",",dtype=str)
-df_train = df_train.sample(n=300, replace = False)
-# print(df_train)
-
-# Get entity spans
-df_entity_spans= create_entity_spans(df_train.astype(str),tag_list)
-training_data= df_entity_spans.values.tolist()
-
-# Get & Persist DocBin to disk
-doc_bin_train= get_doc_bin(training_data,nlp)
-doc_bin_train.to_disk("./corpus/spacy-docbins/train.spacy")
-######################################
-
-
-###### Validation dataset prep ###########
-# Read the validation dataset into pandas
-# df_test=pd.read_csv(filepath_or_buffer="./corpus/dataset/us-test-dataset.csv",sep=",",dtype=str)
-df_test = pd.read_csv(r'corpus/dataset/distinct_addresses.csv',sep=",",dtype=str)
-df_test = df_test.sample(n=50, replace = False)
-# print(df_test)
-
-# Get entity spans
-df_entity_spans= create_entity_spans(df_test.astype(str),tag_list)
-validation_data= df_entity_spans.values.tolist()
-
-# Get & Persist DocBin to disk
-doc_bin_test= get_doc_bin(validation_data,nlp)
-doc_bin_test.to_disk("./corpus/spacy-docbins/test.spacy")
-##########################################
+print("Model best")
+# Checking predictions for the NER model
+for address in address_list:
+    doc=nlp(address)
+    ent_list=[(ent.text, ent.label_) for ent in doc.ents]
+    print("Address string -> "+address)
+    print("Parsed address -> "+str(ent_list))
+    print("******")
--- training_data_prep.py
+++ training_data_prep.py
@@ -2,14 +2,17 @@ import spacy
 from spacy.tokens import DocBin
 import pandas as pd
 import re
-pd.set_option('display.max_colwidth', -1)
+# pd.set_option('display.max_colwidth', -1)
 
 def massage_data(address):
     '''Pre process address string to remove new line characters, add comma punctuations etc.'''
-    cleansed_address1=re.sub(r'(,)(?!\s)',', ',address)
-    cleansed_address2=re.sub(r'(\\n)',', ',cleansed_address1)
-    cleansed_address3=re.sub(r'(?!\s)(-)(?!\s)',' - ',cleansed_address2)
-    cleansed_address=re.sub(r'\.','',cleansed_address3)
+    cleansed_address = re.sub(r'(,)(?!\s)', ', ', address)  # Add comma after comma without space
+    cleansed_address = re.sub(r'(\\n)', ', ', cleansed_address)  # Replace newline characters with comma and space
+    cleansed_address = re.sub(r'(?!\s)(-)(?!\s)', ' - ', cleansed_address)  # Add space around hyphens
+    cleansed_address = re.sub(r'\.', '', cleansed_address)  # Remove periods
+    cleansed_address = re.sub(r'\'', '', cleansed_address)  # Remove single quotes
+    cleansed_address = re.sub(r'[\(\)]', '', cleansed_address)  # Remove parentheses
+    print("After cleanse address: " + cleansed_address)
     return cleansed_address
 
 def get_address_span(address=None,address_component=None,label=None):
@@ -19,10 +22,25 @@ def get_address_span(address=None,address_component=None,label=None):
     if pd.isna(address_component) or str(address_component)=='nan':
         pass
     else:
-        address_component1=re.sub('\.','',address_component)
-        address_component2=re.sub(r'(?!\s)(-)(?!\s)',' - ',address_component1)
-        span=re.search('\\b(?:'+address_component2+')\\b',address)
-        #print(span)
+        # print(f'- full address - {address}')
+        address_component = re.sub(r'(?!\s)(-)(?!\s)',' - ', address_component) # Put space before and after the hyphen
+        address_component = re.sub(r'\.', '', address_component)  # Remove periods from address_component
+        address_component = re.sub(r'\'', '', address_component)  # Remove single quotes from address_component
+        address_component = re.sub(r'[\(\)]', '', address_component)  # Remove parentheses from address_component
+        # print(address_component3)
+        if label == 'CITY' or label == 'ADDITION':
+            print(f'looking for {address_component} into {address}')
+            pattern = rf'\b(?:{address_component})\b'
+            matches = re.finditer(pattern, address)
+            span = None
+            for match in matches:
+                print(f'match found: {match}')
+                span = match
+            print(f'city or addition - found: {span}')
+        else:
+            print(f'looking for {address_component} into {address}')
+            span = re.search('\\b(?:' + address_component + ')\\b', address)
+            print(f'else - found: {span}')
         return (span.start(),span.end(),label)
 
 def extend_list(entity_list,entity):
@@ -34,20 +52,19 @@ def extend_list(entity_list,entity):
 
 def create_entity_spans(df,tag_list):
     '''Create entity spans for training/test datasets'''
-    df['Address']=df['Address'].apply(lambda x: massage_data(x))
-    df["BuildingTag"]=df.apply(lambda row:get_address_span(address=row['Address'],address_component=row['Building_Name'],label='BUILDING_NAME'),axis=1)
-    df["BuildingNoTag"]=df.apply(lambda row:get_address_span(address=row['Address'],address_component=row['Building_Number'],label='BUILDING_NO'),axis=1)
-    df["RecipientTag"]=df.apply(lambda row:get_address_span(address=row['Address'],address_component=row['Recipient'],label='RECIPIENT'),axis=1)
-    df["StreetNameTag"]=df.apply(lambda row:get_address_span(address=row['Address'],address_component=row['Street_Name'],label='STREET_NAME'),axis=1)
-    df["ZipCodeTag"]=df.apply(lambda row:get_address_span(address=row['Address'],address_component=row['Zip_Code'],label='ZIP_CODE'),axis=1)
-    df["CityTag"]=df.apply(lambda row:get_address_span(address=row['Address'],address_component=row['City'],label='CITY'),axis=1)
-    df["CountryTag"]=df.apply(lambda row:get_address_span(address=row['Address'],address_component=row['Country'],label='COUNTRY'),axis=1)
-    df["StateTag"]=df.apply(lambda row:get_address_span(address=row['Address'],address_component=row['State'],label='STATE'),axis=1)
+    df['full_address']=df['full_address'].apply(lambda x: massage_data(x))
+    # df['full_address']=df['full_address']
+    df["straatTag"]=df.apply(lambda row:get_address_span(address=row['full_address'],address_component=row['straat'],label='STREET'),axis=1)
+    df["huisnummerTag"]=df.apply(lambda row:get_address_span(address=row['full_address'],address_component=row['huisnummer'],label='HOUSE_NUMBER'),axis=1)
+    df["huisletterTag"]=df.apply(lambda row:get_address_span(address=row['full_address'],address_component=row['huisletter'],label='HOUSE_LETTER'),axis=1)
+    df["huistoevoegingTag"]=df.apply(lambda row:get_address_span(address=row['full_address'],address_component=row['huistoevoeging'],label='ADDITION'),axis=1)
+    df["postcodeTag"]=df.apply(lambda row:get_address_span(address=row['full_address'],address_component=row['postcode'],label='POSTAL_CODE'),axis=1)
+    df["cityTag"]=df.apply(lambda row:get_address_span(address=row['full_address'],address_component=row['woonplaats'],label='CITY'),axis=1)
     df['EmptySpan']=df.apply(lambda x: [], axis=1)
 
     for i in tag_list:
         df['EntitySpans']=df.apply(lambda row: extend_list(row['EmptySpan'],row[i]),axis=1)
-        df['EntitySpans']=df[['EntitySpans','Address']].apply(lambda x: (x[1], x[0]),axis=1)
+        df['EntitySpans']=df[['EntitySpans','full_address']].apply(lambda x: (x[1], x[0]),axis=1)
     return df['EntitySpans']
 
 def get_doc_bin(training_data,nlp):
@@ -57,22 +74,27 @@ def get_doc_bin(training_data,nlp):
     for text, annotations in training_data:
         doc = nlp(text) #Construct a Doc object
         ents = []
+        print(f'{text} - Annotations: {annotations}')
         for start, end, label in annotations:
             span = doc.char_span(start, end, label=label)
+            print(f'span: {span}')
             ents.append(span)
         doc.ents = ents
         db.add(doc)
     return db
 
-#Load blank English model. This is needed for initializing a Document object for our training/test set.
-nlp = spacy.blank("en")
+#Load blank Dutch model. This is needed for initializing a Document object for our training/test set.
+nlp = spacy.blank("nl")
 
 #Define custom entity tag list
-tag_list=["BuildingTag","BuildingNoTag","RecipientTag","StreetNameTag","ZipCodeTag","CityTag","StateTag","CountryTag"]
+tag_list=["straatTag","huisnummerTag","huisletterTag","huistoevoegingTag","postcodeTag","cityTag"]
 
 ###### Training dataset prep ###########
 # Read the training dataset into pandas
-df_train=pd.read_csv(filepath_or_buffer="./corpus/dataset/us-train-dataset.csv",sep=",",dtype=str)
+# df_train=pd.read_csv(filepath_or_buffer="./corpus/dataset/us-train-dataset.csv",sep=",",dtype=str)
+df_train = pd.read_csv(r'corpus/dataset/distinct_addresses.csv',sep=",",dtype=str)
+df_train = df_train.sample(n=300, replace = False)
+# print(df_train)
 
 # Get entity spans
 df_entity_spans= create_entity_spans(df_train.astype(str),tag_list)
@@ -86,7 +108,10 @@ doc_bin_train.to_disk("./corpus/spacy-docbins/train.spacy")
 
 ###### Validation dataset prep ###########
 # Read the validation dataset into pandas
-df_test=pd.read_csv(filepath_or_buffer="./corpus/dataset/us-test-dataset.csv",sep=",",dtype=str)
+# df_test=pd.read_csv(filepath_or_buffer="./corpus/dataset/us-test-dataset.csv",sep=",",dtype=str)
+df_test = pd.read_csv(r'corpus/dataset/distinct_addresses.csv',sep=",",dtype=str)
+df_test = df_test.sample(n=50, replace = False)
+# print(df_test)
 
 # Get entity spans
 df_entity_spans= create_entity_spans(df_test.astype(str),tag_list)
